f557372c51431529e0fdbbfe351660d4ed4da4dd
Alexandr <alex@webgoal.ru>
2023-08-03 20:08:31 +0300

    Update

--- Less04_2/Less04_2.cpp
+++ Less04_2/Less04_2.cpp
@@ -6,12 +6,6 @@
 class Address
 {
 public:
-	Address() {
-		city_ = "";
-		street_ = "";
-		home_ = 0;
-		apartment_ = 0;
-	};
 	Address(std::string& city, std::string& street, int& home, int& apartment) {
 		city_ = city;
 		street_ = street;
@@ -29,15 +23,15 @@ private:
 	int apartment_; // Номер квартиры
 };
 
-void sort(Address* addresses, int size) {
+void sort(Address **addresses, int size) {
 	// Сортировка массива пузырьком
 	for (int i = 0; i < size - 1; i++) {
 		for (int j = 0; j < size - i - 1; j++) {
-			char a = addresses[j].get_city()[0];
-			char b = addresses[j + 1].get_city()[0];
+			char a = addresses[j]->get_city()[0];
+			char b = addresses[j + 1]->get_city()[0];
 			if (a > b) {
 				// меняем элементы местами
-				Address temp = addresses[j];
+				Address * temp = addresses[j];
 				addresses[j] = addresses[j + 1];
 				addresses[j + 1] = temp;
 			}
@@ -61,7 +55,7 @@ int main()
 
 	if (size_arr > 0)
 	{
-		Address* address = new Address[size_arr];
+		Address** address = new Address*[size_arr];
 
 		for (size_t i = 0; i < size_arr; i++)
 		{
@@ -75,29 +69,26 @@ int main()
 			flie_read >> home;
 			flie_read >> apartment;
 
-			address[i] = Address(city, street, home, apartment);
+			address[i] = new Address(city, street, home, apartment);
 		}
 
 		sort(address, size_arr); // Сортируем масссив
 
 		std::ofstream flie_write("out.txt");
-		if (!flie_write.is_open())
-		{
-			std::cout << "File 'out.txt' not open!\n";
-			delete[] address;
-			return 0;
-		}
 
 		flie_write << size_arr << std::endl;
 
 		for (size_t i = 0; i < size_arr; i++)
 		{
-			flie_write << address[i].get_output_address() << std::endl;
+			flie_write << address[i]->get_output_address() << std::endl;
 		}
 
 		flie_write.close();
 
-
+		for (size_t i = 0; i < size_arr; i++)
+		{
+			delete address[i];
+		}
 		delete[] address;
 
 	}
